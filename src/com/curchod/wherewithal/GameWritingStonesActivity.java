8c21b06d5de59b5446bbd488bc49024c7bcce2d0
timofeysie <timofeyc@hotmail.com>
2013-12-08 18:14:43 +0900

    changed build number of writing stones game to 51

--- src/com/curchod/wherewithal/GameWritingStonesActivity.java
+++ src/com/curchod/wherewithal/GameWritingStonesActivity.java
@@ -195,7 +195,7 @@ public class GameWritingStonesActivity extends Activity implements View.OnClickL
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_writing_stones);
 		String method = "onCreate";
-		String build = "build 50c";
+		String build = "build 51";
 		Log.i(DEBUG_TAG, method+": "+build);
 		setup();
 		getIntentInfo();
